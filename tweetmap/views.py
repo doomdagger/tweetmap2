ff50709adff98dd82b9f751f33c50644eea951bb
He Li <jet.in.brain@gmail.com>
2016-03-16 01:00:49 -0400

    add message type

--- tweetmap/views.py
+++ tweetmap/views.py
@@ -12,13 +12,24 @@ def index():
 
 @app.route('/sns', methods=['POST'])
 def sns():
+    if u'x-amz-sns-message-type' not in request.headers:
+        abort(400)
+    message_type = request.headers[u'x-amz-sns-message-type']
     # validate msg source
     if u'x-amz-sns-topic-arn' not in request.headers or request.headers[u'x-amz-sns-topic-arn'] != sns_msg_source:
         abort(400)
-    # get parsed sns_msg
-    sns_msg = request.get_json()
-    if u'Message' not in sns_msg:
-        abort(400)
-    # parse original message
-    orig_msg = json.loads(sns_msg[u'Message'])
-    es.create(index='test-index', doc_type='test-type', body=orig_msg)
+    if message_type == 'Notification':
+        # get parsed sns_msg
+        sns_msg = request.get_json()
+        if u'Message' not in sns_msg:
+            abort(400)
+        # parse original message
+        orig_msg = json.loads(sns_msg[u'Message'])
+        es.create(index='test-index', doc_type='test-type', body=orig_msg)
+    elif message_type == 'SubscriptionConfirmation':
+        print 'SubscriptionConfirmation request'
+    elif message_type == 'UnsubscribeConfirmation':
+        print 'UnsubscribeConfirmation request'
+    else:
+        print 'Unknown request'
+    return 200
