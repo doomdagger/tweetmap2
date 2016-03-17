2943a1fef8717ab68b158c85b325264d8fa39802
He Li <jet.in.brain@gmail.com>
2016-03-16 23:44:52 -0400

    finish updating

--- application.py
+++ application.py
@@ -9,4 +9,4 @@ __author__ = "He Li"
 logging.basicConfig(filename='tweetmap2.log', level=logging.DEBUG)
 
 if __name__ == '__main__':
-    io.run(app, host='0.0.0.0', port='5000', debug=True)
+    io.run(app, host='0.0.0.0', port=5000, debug=True)
